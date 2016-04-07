fe2354fd130179a25451f01a36ad4db4fe2e3ce4
Ties van Rozendaal <git@tivaro.nl>
2016-04-07 21:31:07 +0200

    Debug printing, also boundaries now ranging from 1 - len(utt)-1

--- segmented_corpus.py
+++ segmented_corpus.py
@@ -12,11 +12,12 @@ class segmented_corpus:
 	boundary_count  = 0
 
 	utterances = ['hoihoeishet','watisdit'] #list of utterances (unseparated)
-	boundaries = [[2,5,7],[3,4]] #list of list. indexed by utterance_id each entry containing a list with the positions of the boundaries
+	boundaries = [[3,6,8],[2,4]] #list of list. indexed by utterance_id each entry containing a list with the positions of the boundaries (range 1-len(utterance)-1)
 
 
 	#list of tuples (utterance_id, boundary_id) used for sampling
-	sample_list = [(u,b) for u, utterance in enumerate(utterances) for b in range(len(utterance) - 1)]
+	sample_list = [(u,b) for u, utterance in enumerate(utterances) for b in range(1, len(utterance) )]
+	print sample_list
 	
 
 	def __init__(self):
@@ -26,7 +27,7 @@ class segmented_corpus:
 		pass
 
 
-	def gibs_sampler(self):
+	def gibs_sampler(self, debug=None):
 
 		#parameters:
 		#temperature = 1 #temperature (annealing)
@@ -36,25 +37,27 @@ class segmented_corpus:
 
 
 		#Randomly draw boundary location
-		u, boundary = self.sample_list[np.random.randint(len(self.sample_list))]
+		u, boundary = debug or self.sample_list[np.random.randint(len(self.sample_list))]
 		utterance   = self.utterances[u]
 		boundaries  = self.boundaries[u]
 
 		#Find previous and next boundary
-		prev_b, next_b, boundary_exists, insert_boundary_at = segmented_corpus.neighbours(boundaries, boundary) 
-		prev_b = prev_b or -1
-		next_b = next_b or len(utterance) - 1
+		prev_b, next_b, boundary_exists, insert_boundary_at = segmented_corpus.neighbours(boundaries, boundary)
+		prev_b = prev_b or 0
 
 		#find start, end 
-		w1 = utterance[prev_b + 1:next_b]
+		w1 = utterance[prev_b:next_b]
 		w2 = w1[:boundary-prev_b]
 		w3 = w1[boundary-prev_b:]
 
-		print " " * boundary + 'V' + ' ' * int(boundary_exists) + 'V'
-		print segmented_corpus.insert_boundaries(utterance, boundaries)
-		print w1
-		print w2
-		print w3
+		if debug:
+			print "     " + " " * (boundary - 1 + insert_boundary_at - boundary_exists) + 'V' + ' ' * int(boundary_exists) + 'V'
+			print "utt: " + segmented_corpus.insert_boundaries(utterance, boundaries)
+			boundaries.insert(insert_boundary_at, boundary)
+			boundaries.pop(insert_boundary_at)
+			print "w1: " + w1
+			print "w2: " + w2
+			print "w3: " + w3
 
 
 		#calculate P1
@@ -115,8 +118,12 @@ class segmented_corpus:
 
 	@staticmethod
 	def insert_boundaries(utterance, boundaries, delimiter = '.'):
-		return delimiter.join([utterance[i:j] for i, j in zip([0]+boundaries, boundaries+[None])])
+		return delimiter.join([utterance[(i):(j)] for i, j in zip([0]+boundaries, boundaries+[None])])
 
 
 s= segmented_corpus()
-s.gibs_sampler()			
+s.gibs_sampler((1,1) )
+s.gibs_sampler((1,2) )
+s.gibs_sampler((1,3) )
+s.gibs_sampler((1,5) )
+s.gibs_sampler((1,6) )
