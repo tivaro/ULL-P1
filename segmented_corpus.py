0866666cffd4750c1c25cc422fed9260139cdf9b
Ties van Rozendaal <git@tivaro.nl>
2016-04-07 13:49:47 +0200

    Did more implementeation

--- segmented_corpus.py
+++ segmented_corpus.py
@@ -3,13 +3,16 @@ class segmented_corpus:
 
 	"""
 
-	words_$ = Counter() #word counts at utterance boundary
-	words_u = Counter() #other word counts
+	words_$ 		= Counter() #word counts at utterance boundary
+	words_u 		= Counter() #other word counts
+	boundary_count  = 0
 
-	utterances = [] #list of utterances (unseparated)
-	boundaries = [] #list of list of indexes of utterance boundaries (indexed by utterance)
+	utterances = ['hoihoeishet','watisdit'] #list of utterances (unseparated)
+	boundaries = [[2,5,7],[3,4]] #list of list. indexed by utterance_id each entry containing a list with the positions of the boundaries
 
-	boundary_ids[] #a continious list of (utterance_id, boundary_id)
+
+	#boundary_ids[] #list of tuples (utterance_id, boundary_id) used for sampling
+	sample_list = [(x,n) for n, utterance in enumerate(utterances) for x in range(len(utterance))]
 	
 
 	def __init__(self):
@@ -18,31 +21,46 @@ class segmented_corpus:
 	def load_segmented_corpus(filename, utterance_delimiter = '\n', word_delimiter = ' '):
 		pass
 
-
-def gibs_sampler():
-	pass
-
-	#parameters:
-	temperature = 1 #temperature (annealing)
-	alpha0 = #dirichlet concentration
-	tau = #
-	p$ #word ending probability
+	def initialize_boundaries_randomly():
+		pass
 
 
-	#Randomly draw boundary location
-	w1 = #string from previous to next boundary location (or uterance end)
-	w2 = #split w1 by proposed boundary, first half 
-	w3 = #split w1 by proposed boundary, remainder 
+	def gibs_sampler():
+
+		#parameters:
+		temperature = 1 #temperature (annealing)
+		alpha0 = #dirichlet concentration
+		rho = #
+		p$ #word ending probability
 
 
-	#calculate P1
+		#Randomly draw boundary location
+		w1 = #string from previous to next boundary location (or uterance end)
+		w2 = #split w1 by proposed boundary, first half 
+		w3 = #split w1 by proposed boundary, remainder 
 
-	#calculate P2
 
-	#sample proportionally
+		#calculate P1
 
-	if changed:
-		#update counts
+		#calculate P2
 
-		#update boundary
+		#sample proportionally
+
+		if changed:
+			#update counts
+			if word is utterance end:
+				#put in words_$
+			else:
+				#put in words_u
+
+			#update boundary (and boundary_count)
+
+
+		#Hypothesis 2: A  boundary: split words w2 and w3
+
+		# h- : all words in the corpus except w1, w2 and w3
+
+		#w1 in h- + alpha0 * P(w1)   #nu in h- + rho/2
+		#------------------------  x ------------------
+	#        total(h-)            total(h-) + tau
 
