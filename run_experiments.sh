b6a003019a5c6e64f669ea7fd04878ff85e5e948
Ties van Rozendaal <git@tivaro.nl>
2016-04-13 14:28:39 +0200

    Added batch script

---
+++ run_experiments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p cpu
+#SBATCH -N 1
+#SBATCH -t 22:00:00
+module load python
+
+if [ srun -u python "SVM.py" ]; then
+	mailx -s "Experiment completed\! :)" < /dev/null "tivarosite@gmail.com"
+else
+	mailx -s "Experiment failed :(" < /dev/null "tivarosite@gmail.com"
+fi
+
+sleep 20
