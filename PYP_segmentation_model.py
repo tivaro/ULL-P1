ccc86544cb9beb7674dcfa3e05b2a4e2370c47cb
Ties van Rozendaal <git@tivaro.nl>
2016-04-14 15:25:04 +0200

    Divided the DP and PYP into subclasses of a general model class

--- PYP_segmentation_model.py
+++ PYP_segmentation_model.py
@@ -1,3 +1,4 @@
+import word_segmentation
 from collections import defaultdict, Counter
 from random import random
 from iProgressBar import ProgressBar
@@ -5,63 +6,24 @@ import numpy as np
 import utils
 
 
-class segmented_corpus:
+class PYP_word_segmentation_model(word_segmentation.Word_segmentation_model):
 	"""
-
+	Word segementation based on a unigram PYP model
 	"""
 
 	__version__ = 0.8
 
 	#parameters
-	alpha0 = 1 #dirichlet concentration
+	alpha = 1 #dirichlet concentration
 	beta = 0.2 #PYP beta
-	temp_regime = (20000, np.arange(0.1,1.1,0.1)) #temperature steps for gibbs sampling (annealing) in a tuple (total_iterations, regime)
-	rho = 2 #parameter of beta prior over utterance end
-	p_dash = 0.5 #probability of ending a word (in P_0)
-
 
 	# The seating arrangement
 	seating		 	 = defaultdict(lambda: np.array([])) #indexed by word, then a list of table counts (e.g. seating['no'] = [25,4,1] means three tables and 30 occurances)
 	K 			 	 = 0 #Total number of tables
-	total_word_count = 0
 
-	phoneme_counts = Counter()
-	total_phoneme_count = 0
-	P0_cache = {}
 
-	utterances = [] #list of utterances (unseparated)
-	boundaries = [] #list of list. indexed by utterance_id each entry containing a list with the positions of the boundaries (range 1-len(utterance)-1)
-
-	# for evaluation purposes
-	original_word_counts = defaultdict(lambda: np.array([]))
-	original_seating = []
-
-	#list of tuples (utterance_id, boundary_id) used for sampling
-	sample_list = []
-
-	P0_method = None
-
-	def __init__(self, corpusfile=None, temp_regime_id=0, P0_method='uniform'):
-
-		if corpusfile:
-			utterances, boundaries = utils.load_segmented_corpus(corpusfile)
-			self.initialize_lexicon(utterances, boundaries)
-
-			self.original_boundaries = self.boundaries[:]
-			self.original_seating = self.seating.copy()
-
-		#temp_regimes decides the amount of iterations and the temperature regime
-		temp_regimes = []
-		temp_regimes.append([20000, np.arange(0.1,1.1,0.1)])
-		temp_regimes.append([30000, np.arange(0.1,1.6,0.1)])
-		temp_regimes.append([40000, np.arange(0.002,1.002,0.002)])
-		self.temp_regime = temp_regimes[temp_regime_id]
-		#compute the time steps when we have to change temperatures
-		self.temp_regime.append(int(self.temp_regime[0]/len(self.temp_regime[1])))
-		self.temp_regime = tuple(self.temp_regime)
-		#print(self.temp_regime)
-
-		self.P0_method = P0_method # 'mle' or 'uniform'
+	def getWordCounts(self):
+		return self.seating
 
 	def initialize_lexicon(self, utterances, boundaries):
 
@@ -79,7 +41,7 @@ class segmented_corpus:
 		self.phoneme_counts   = Counter()
 
 		for utterance, ut_boundaries in zip(utterances, boundaries):
-			words = segmented_corpus.split_utterance(utterance, ut_boundaries)
+			words = word_segmentation.split_utterance(utterance, ut_boundaries)
 			self.add_customer(words)
 
 			for phonemes in map(list, words):
@@ -98,7 +60,7 @@ class segmented_corpus:
 			tables = (self.seating[word_or_words] - self.beta).clip(min=0)
 
 			# Calculate the probability for a new table (append to beginnning)
-			tables = np.append(self.alpha0 + (self.beta * self.K), tables)
+			tables = np.append(self.alpha + (self.beta * self.K), tables)
 
 			# Normalise probabilities
 			tables = tables / sum(tables)
@@ -133,49 +95,6 @@ class segmented_corpus:
 			self.total_word_count -= 1
 
 
-	def initialize_boundaries_randomly(self, boudary_proportion=0.3):
-
-		#Remove all boundaries and import sample list, utternaces and boundaries
-		self.remove_all_boundaries()
-		sample_list = self.sample_list
-		boundaries  = self.boundaries
-		utterances = self.utterances
-
-		#randomly select the defined proportion of boundaries
-		boundary_ids = range(len(sample_list))
-		np.random.shuffle(boundary_ids)
-		to = int(round(len(sample_list) * boudary_proportion))
-		chosen_boundaries = boundary_ids[:to]
-
-		#sort the list again because the boundaries stored in the object have to be sorted.
-		chosen_boundaries.sort()
-
-		#append the chosen boundaries to the (now initialised empty boundary list)
-		[boundaries[sample_list[i][0]].append(sample_list[i][1]) for i in chosen_boundaries]
-
-		#reinitialise to set the right counts
-		self.initialize_lexicon(utterances, boundaries)
-
-	def remove_all_boundaries(self):
-		utterances = self.utterances
-		boundaries = [[] for _ in range(len(utterances))]
-		self.initialize_lexicon(utterances, boundaries)
-
-	def P_phoneme(self, x):
-		if self.P0_method == 'mle':
-			return self.phoneme_counts[x] / float(self.total_phoneme_count)
-
-		return 1.0 / float(self.unique_phoneme_count)
-
-	def P0(self, word):
-		if word not in self.P0_cache:
-			M = len(word)
-			Pphoneme = np.prod([self.P_phoneme(x) for x in list(word)])
-
-			self.P0_cache[word] = self.p_dash * ((self.p_dash)**(M-1)) * Pphoneme
-
-		return self.P0_cache[word]
-
 	def P_corpus(self):
 		"""
 		Returns the joint probability of all words in the corpus
@@ -185,53 +104,16 @@ class segmented_corpus:
 		pws = []
 
 		for word, tables in self.seating.iteritems():
-			pws.append(sum(tables) * (np.log( (self.P0(word) * self.alpha0) + sum(tables) + (self.beta * (self.K - len(tables)) ) )
-								- np.log(self.total_word_count - 1 + self.alpha0)
+			pws.append(sum(tables) * (np.log( (self.P0(word) * self.alpha) + sum(tables) + (self.beta * (self.K - len(tables)) ) )
+								- np.log(self.total_word_count - 1 + self.alpha)
 								) )
 		return sum(pws)
-
-
-	def gibbs_sampler(self, log=[], debug=None):
-		"""
-		Performs gibs sampling according to the iteration/temperature scheme in s.temp_regime
-		the variable log should be a list with values that should be logged after each iteration (the log is returned)
-		possible elements are:
-			'P_corpus': the probabilty of the entire corpus
-			'n_types': the lexicon size
-			'n_tokens': the number of tokens
-			'temperature': the temperature used at each iteration
-		"""
-
-		iterations, temp_steps, step_size = self.temp_regime
-		p = ProgressBar(iterations)
-
-		# initialize log
-		logs = {var:[] for var in log}
-
-		for i in range(iterations):
-
-			#update temperature
-			if i % step_size == 0:
-				temperature = 1/temp_steps[i/step_size]
-
-			#do one iteration and update progressbar
-			self.gibbs_sample_once(debug=debug, temperature=1)
-			p.animate(i)
-
-			#update log
-			if 'P_corpus'    in log: logs['P_corpus'].append( self.P_corpus())
-			if 'n_types'     in log: logs['n_types'].append( len(self.word_counts))
-			if 'n_tokens'    in log: logs['n_tokens'].append( self.total_word_count)
-			if 'temperature' in log: logs['temperature'].append( temperature)
-
-
-		if log: return logs
 
 
 	def gibbs_sample_once(self, temperature=1, debug=None):
 
 		#import parameters:
-		alpha0 = self.alpha0
+		alpha = self.alpha
 		rho    = self.rho
 		beta   = self.beta
 
@@ -245,7 +127,7 @@ class segmented_corpus:
 			boundaries  = self.boundaries[u]
 
 			#Find previous and next boundary
-			prev_b, next_b, boundary_exists, insert_boundary_at = segmented_corpus.neighbours(boundaries, boundary)
+			prev_b, next_b, boundary_exists, insert_boundary_at = word_segmentation.neighbours(boundaries, boundary)
 			prev_b = prev_b or 0
 			utterance_final = not next_b
 
@@ -280,12 +162,12 @@ class segmented_corpus:
 			n_utter_ends = len(self.utterances) - int(utterance_final)
 			nu = n_utter_ends if utterance_final else n_total - n_utter_ends
 
-			p_no_boundary = (1.0 * (nw1 + alpha0 * self.P0(w1) + beta * (K - kw1)) / (n_total + alpha0)) * \
+			p_no_boundary = (1.0 * (nw1 + alpha * self.P0(w1) + beta * (K - kw1)) / (n_total + alpha)) * \
 							(1.0 * (nu + (rho/2)) / (n_total + rho))
 
-			p_boundary =	(1.0 * (nw2 + alpha0 * self.P0(w2)  + beta * (K - kw2)) / (n_total + alpha0)) * \
+			p_boundary =	(1.0 * (nw2 + alpha * self.P0(w2)  + beta * (K - kw2)) / (n_total + alpha)) * \
 							(1.0 * (n_total - n_utter_ends+ (rho/2)) / (n_total + rho) ) * \
-							(1.0 * (nw3 + int(w2 == w3) + alpha0 * self.P0(w3)  + beta * (K - kw3))/ (n_total + 1 + alpha0)) * \
+							(1.0 * (nw3 + int(w2 == w3) + alpha * self.P0(w3)  + beta * (K - kw3))/ (n_total + 1 + alpha)) * \
 							(1.0 * (nu  + int(w2 == w3) + (rho/2)) / (n_total + 1 + rho))
 
 			# Modify probabilities using annealing
@@ -318,74 +200,10 @@ class segmented_corpus:
 					self.boundaries[u].pop(insert_boundary_at)
 
 
-	def evaluate(self):
-		"""
-		Returns a dict of tuples (precicion, recall, F0) for:
-		'words',
-		'boundaries',
-		'lexicon'
-		"""
-		words = utils.eval_words(self.boundaries, self.original_boundaries)
-		boundaries = utils.eval_boundaries(self.boundaries, self.original_boundaries)
-		lexicon = utils.eval_lexicon(self.seating, self.original_seating)
-
-		return {'words': words, 'boundaries':boundaries, 'lexicon':lexicon}
-
-
-	def print_evaluation(self):
-		evaluation = self.evaluate()
-
-		print '=== Evaluation ========================'
-		print 'Pw = %.3f, Rw = %.3f, Fw = %.3f' % evaluation['words']
-		print 'Pb = %.3f, Rb = %.3f, Fb = %.3f' % evaluation['boundaries']
-		print 'Pl = %.3f, Rl = %.3f, Fl = %.3f' % evaluation['lexicon']
-
-	@staticmethod
-	def neighbours(sorted_list, entry, full_description = True):
-		"""
-		Input: sorted list, entry (a possible feature enty)
-		Output: (prev_neighbour, next_neighbour, entry_exists, insert_entry)
-		where insert_entry indicates the place to insert the entry to keep the list sorted
-		"""
-
-		prev_neighbour = None
-		next_neighbour = None
-		entry_exists   = False
-		insert_entry   = len(sorted_list)
-
-
-		for n,e in enumerate(sorted_list):
-
-			if e < entry:
-				prev_neighbour = e
-			elif e == entry:
-				entry_exists = True
-			else:
-				next_neighbour = e
-				insert_entry = n
-				break
-
-		#insert_entry should equal the next element if entry does not exit, but it's true location if entry exists
-		insert_entry -= int(entry_exists)
-
-		if full_description:
-			return prev_neighbour, next_neighbour, entry_exists, insert_entry
-		else:
-			return prev_neighbour, next_neighbour
-
-	@staticmethod
-	def split_utterance(utterance, boundaries):
-		return [utterance[(i):(j)] for i, j in zip([0]+boundaries, boundaries+[None])]
-
-	@staticmethod
-	def insert_boundaries(utterance, boundaries, delimiter = '.'):
-		return delimiter.join(segmented_corpus.split_utterance(utterance, boundaries))
-
-
 
 ### DEMO CODE ###
 def gibbs_demo():
-	s = segmented_corpus('br-phono-train.txt')
+	s = PYP_word_segmentation_model('br-phono-train.txt')
 
 	print 'Init correctely'
 	for _ in range(4):
@@ -419,19 +237,19 @@ def gibbs_demo():
 
 
 def boundary_reset_test():
-	s = segmented_corpus('br-phono-toy.txt')
+	s = PYP_word_segmentation_model('br-phono-toy.txt')
 	s.gibbs_sample_once(debug=(1,1) )
 	s.remove_all_boundaries()
 	s.gibbs_sample_once(debug=(1,1) )
 
 def boundary_random_test():
-	s = segmented_corpus('br-phono-toy.txt')
+	s = PYP_word_segmentation_model('br-phono-toy.txt')
 	s.gibbs_sample_once(debug=(1,1) )
 	s.initialize_boundaries_randomly()
 	s.gibbs_sample_once(debug=(1,1) )
 
 def gibbs_test():
-	s = segmented_corpus('br-phono-toy.txt')
+	s = PYP_word_segmentation_model('br-phono-toy.txt')
 	s.remove_all_boundaries()
 	s.gibbs_sampler()
 
@@ -444,7 +262,7 @@ def gibbs_test():
 	s.print_evaluation()
 
 def joint_prop_test():
-	s = segmented_corpus('br-phono-toy.txt')
+	s = PYP_word_segmentation_model('br-phono-toy.txt')
 	print s.P_corpus()
 	s.remove_all_boundaries()
 	print s.P_corpus()
@@ -456,7 +274,7 @@ def gibbs_log_demo():
 	Shows the effect of gibs sampling iterations
 	"""
 	import matplotlib.pyplot as plt
-	s = segmented_corpus('br-phono-toy.txt')
+	s = PYP_word_segmentation_model('br-phono-toy.txt')
 	s.remove_all_boundaries()
 	logs = s.gibbs_sampler(log=['P_corpus', 'n_types', 'n_tokens'])
 
@@ -479,7 +297,7 @@ def gibbs_log_demo():
 	plt.show()
 
 def eval_demo():
-	s = segmented_corpus('br-phono-toy.txt')
+	s = PYP_word_segmentation_model('br-phono-toy.txt')
 	s.print_evaluation()
 	s.remove_all_boundaries()
 	s.print_evaluation()
@@ -498,6 +316,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-
-
