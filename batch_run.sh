0330a8ea28c23d6c3f7e31b0b4c6a15c2dac2dda
Ties van Rozendaal (Cartesius) <sdemo029@int2.cartesius.surfsara.nl>
2016-04-15 14:49:54 +0200

    Batch script is now only adding python jobs when a lockfile has been created

--- batch_run.sh
+++ batch_run.sh
@@ -5,9 +5,24 @@ if [ -f "$file" ]
 	rm "$file"
 fi
 
-for i in {0..30..1}
+export maxTimeout=600
+
+export timeout=0
+export no_old_files=-1
+
+while [ $timeout -le $maxTimeout ]
 do
-echo "Starting up node $i"
-sbatch run_experiments.sh
+export no_new_files="$(ls results -1 | wc -l)"
+if [ $no_old_files -ne $no_new_files ];
+then
+	echo "Files = $no_new_files"
+	echo "Starting up node $i"
+	sbatch run_experiments.sh
+	export no_old_files=$no_new_files
+	export timeout=0
+fi
+timeout=$((timeout+10))
 sleep 10
 done
+
+echo "File creation timed out ($maxTimeout sec)"
